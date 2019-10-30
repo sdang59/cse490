28f5c791dfa9abadc9bb0640bcdb2bb8cbe44dbe
sdang59 <shayla.dang14@gmail.com>
2019-10-30 14:37:36 -0700

    hw#5-final
    still need to fix lighting issues, currently working on it. got the scripts in

---
+++ sdang59-vr-project/Assets/Scripts/MERP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MERP : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+    if (Input.GetKey(KeyCode.W))
+        { rigid.MovePosition(transform.position + this.transform.forward * speed * Time.deltaTime);  }
+    else if (Input.GetKey(KeyCode.S))
+        { rigid.MovePosition(transform.position + -this.transform.forward * speed * Time.deltaTime);
+        }
+    if (Input.GetKey(KeyCode.A))
+        { rigid.MovePosition(transform.position + -transform.right * speed * Time.deltaTime);
+        }
+    if (Input.GetKey(KeyCode.D))
+            { rigid.MovePosition(transform.position + transform.right * speed * Time.deltaTime); }
+    }
+}
